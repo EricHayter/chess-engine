b34b6250b8da297bc6e1d111c67c2ac5ec8582cf
Eric <hayter.eric@gmail.com>
2024-01-01 10:02:43 -0500

    big bugs are gone in arraylist almost completed

--- arraylist.c
+++ arraylist.c
@@ -2,13 +2,15 @@
 #include <stdint.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <sys/types.h>
+#include <limits.h>
 
 #define DEFAULT_CAPACITY 10
-#define MAX_CAPACTIY sizeof(uint32_t) * 8 - 1
+#define MAX_CAPACTIY UINT_MAX - 1
 #define GROWTH_RATE 2 // how much to increase the size of the list by
                       // if the capacity is exceeded
 
-// add some errors
+int i = 0;
 
 typedef struct {
     size_t data_size;
@@ -22,32 +24,27 @@ ArrayList* arraylist_create(size_t ds) {
     list->data_size = ds;
     list->capacity = DEFAULT_CAPACITY;
     list->size = 0;
-    list->content = malloc(ds * DEFAULT_CAPACITY);
+    list->content = calloc(DEFAULT_CAPACITY, sizeof(void *));
     if (!list->content)
         return NULL;
     return list;
 }
-
-void destroy_list(ArrayList* list) {
-    free(list->content);
-    free(list);
-}
+void destroy_list(ArrayList* list) { free(list->content); free(list); }
 
 int push(ArrayList* list, void* element) {
     assert((list != NULL) && "arraylist pointer is null");
     if (list->capacity == MAX_CAPACTIY)
         return 1; 
 
-    printf("new size: %d\n", (int) (list->capacity * list->data_size));
-    if (list->size == list->capacity) {
+    if (list->size == list->capacity - 1) {
         if (list->capacity >= MAX_CAPACTIY / GROWTH_RATE)
            list->capacity = MAX_CAPACTIY; 
         else
             list->capacity *= GROWTH_RATE;
-        list->content = realloc(list->content, list->capacity * list->data_size);
+        list->content = realloc(list->content, list->capacity * sizeof(void *));
     } 
 
-    list->content[list->size++] = (void *) 6;
+    list->content[list->size++] = (void *) element;
     return 0;
 }
 
@@ -56,8 +53,6 @@ void* pop(ArrayList* list) {
 }
 
 void* get(ArrayList* list, uint32_t idx) {
-    if (idx >= list->size)
-        return NULL;
     return list->content[idx];
 }
 
@@ -66,14 +61,15 @@ void set(ArrayList* list, uint32_t idx, void* element) {
 }
 
 int main() {
-    ArrayList* list = arraylist_create(sizeof(uint8_t));
+    ArrayList* list = arraylist_create(sizeof(int));
     if (!list)
         return 1;
 
 
-    for (uint8_t i = 0; i < 164; i++) {
-        push(list, (void*)(uintptr_t) i); 
+    for (int i = 0; i < 164; i++) {
+        push(list, (void*)(intptr_t) i); 
     } 
 
+    printf("Done\n");
     destroy_list(list);
 }

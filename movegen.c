7c7814e739d492fec9a418b391f40c3d9adc89f7
Eric Hayter <hayter.eric@gmail.com>
2024-03-02 12:09:35 -0500

    going to make move generation generic i.e. should be the same process for all moves

--- movegen.c
+++ movegen.c
@@ -2,9 +2,8 @@
 #include "movelist.h"
 #include "moves.h"
 
-// adds all "basic" moves to the movelist. Loosely using basic as a way of saying
-// non-pawn moves and non castling.
-void add_basic_moves(MoveList *movelist, Board *board, BoardPosition origin)
+// adds all moves to the movelist.
+void add_moves(MoveList *movelist, Board *board, BoardPosition origin)
 {
 
     PieceType type = get_piece_type(board, origin);
@@ -29,6 +28,7 @@ void add_basic_moves(MoveList *movelist, Board *board, BoardPosition origin)
        moves = rook_moves(board, origin);
        break;
     case PAWN: // we don't handle the pawn moves here
+       moves = pawn_moves(board, origin) | pawn_attacks(board, origin);
        return;
     }
 
@@ -46,18 +46,3 @@ void add_basic_moves(MoveList *movelist, Board *board, BoardPosition origin)
 
 }
 
-void add_pawn_moves(MoveList *movelist, Board *board, BoardPosition origin)
-{
-    BitBoard moves = pawn_attacks(origin, board->turn); 
-    BitBoard enemy = get_enemy_squares(board);
-
-    BoardPosition attack_square;
-    while (moves) {
-        attack_square = LSB(moves); 
-        moves = reset_bit(moves, attack_square);
-
-        if (set_bit(0ull, attack_square) & enemy) {
-            movelist_push(movelist, create_move(origin, attack_square, CAPTURE));
-        }
-    }
-}
